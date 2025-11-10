d354e4f08bb69bd09dc9d938589de4fb48950948
Pavilion <Pavilion@LAPTOP-R807H48O>
2025-11-10 20:37:57 +0530

    new class add in practice package

---
+++ src/test/java/PracticeSession/github.java
@@ -0,0 +1,12 @@
+package PracticeSession;
+
+public class github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Good Morning!!!!!");
+
+	}
+
+}
